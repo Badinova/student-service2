d9c7bfa18619b666b8040f6fd668aa7996333f69
badin <badinovahanna@gmail.com>
2025-06-02 13:51:34 +0200

    Complete remove @Autowired, add @RequiredArgsConstructor

--- src/main/java/ait/cohort55/student/service/StudentServiceImpl.java
+++ src/main/java/ait/cohort55/student/service/StudentServiceImpl.java
@@ -7,17 +7,20 @@ import ait.cohort55.student.dto.StudentDto;
 import ait.cohort55.student.dto.StudentUpdateDto;
 import ait.cohort55.student.dto.exception.StudentNotFoundException;
 import ait.cohort55.student.model.Student;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
 import java.util.stream.StreamSupport;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
-    @Autowired
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
 
     @Override
     public Boolean addStudent(StudentAddDto studentAddDto) {
